f32203383b627f409f3db736da0eb7c0c01365e2
agrim saxena <agrim.12saxena@gmail.com>
2024-11-18 12:33:35 +0530

    renderer-db-key

--- database.py
+++ database.py
@@ -5,7 +5,7 @@ from sqlalchemy.orm import sessionmaker, scoped_session
 import os
 
 # Get the database URL from environment variables
-DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://neondb_owner:Yn83ygiUmPWe%40@ep-noisy-tooth-a5rgp2nw-pooler.us-east-2.aws.neon.tech/neondb")
+DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://vehiclerouterdb_user:OGPmKt3f9iCI0q6AwQZXIrYMXjbf8caV@dpg-csteb0jtq21c73a9u6j0-a/vehiclerouterdb")
 
 engine = create_engine(DATABASE_URL)
 
